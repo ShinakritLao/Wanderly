0a71274557987f3847f14b243da91034768b67d7
NotIncluded <ait98761@gmail.com>
2025-10-30 15:13:25 +0700

    test : Software test - Fixing CI
    Closes #6

--- Wanderly/QA/playwright.config.js
+++ Wanderly/QA/playwright.config.js
@@ -27,7 +27,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:8081', // 👈 UNCOMMENTED AND CHANGED TO PORT 8081
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -74,8 +74,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   // webServer: {
   //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
+  //   url: 'http://localhost:8081', // 👈 Also changed to 8081 (if you want to enable this)
   //   reuseExistingServer: !process.env.CI,
   // },
 });
-
