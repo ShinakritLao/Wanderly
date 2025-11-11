452df0d8d81be351ab41f853d35173bed2edf563
NotIncluded <ait98761@gmail.com>
2025-11-11 17:02:00 +0700

    feat: AddNewPlaceFunction - fixing page eror
    Closes #3

--- Wanderly/App.js
+++ Wanderly/App.js
@@ -1,1 +1,15 @@
 
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import AppNavigator from './src/navigation/AppNavigator';
+import { FavoritesProvider } from './src/context/FavoritesContext';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <FavoritesProvider>
+        <AppNavigator />
+      </FavoritesProvider>
+    </NavigationContainer>
+  );
+}
